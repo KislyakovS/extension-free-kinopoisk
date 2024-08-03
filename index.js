ef30af3c91357104c0e1fedd0006f9ac36169c16
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2024-08-03 16:23:25 +0300

    fix: Исправлен переход со странички с посетром

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const HOST = "https://www.sspoisk.ru";
 const TARGET_ELEMENT_SELECT = '[data-tid="f22e0093"]';
 const CLASS_NAME = "link-free-kinoposik";
 
-const getRedirectUrl = () => `${HOST}${window.location.pathname}`;
+const getRedirectUrl = () =>
+  `${HOST}${window.location.pathname.replace("/posters/", "")}`;
 
 const onClickTarget = () => window.open(getRedirectUrl(), "_blank").focus;
 
 const init = () => {
   const target = document.querySelector(TARGET_ELEMENT_SELECT);
-  target.classList.add(CLASS_NAME);
 
-  target?.addEventListener("click", onClickTarget);
+  if (target) {
+    target.classList.add(CLASS_NAME);
+    target.addEventListener("click", onClickTarget);
+  }
 };
 
 init();
